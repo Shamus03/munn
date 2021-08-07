4404ac90a41a58a741875d426550ad6bec33af6e
Shamus Taylor <Shamus03@me.com>
2021-08-07 00:34:48 -0500

    Drop filename completion

--- cmd/munn/cmd_completion.go
+++ cmd/munn/cmd_completion.go
@@ -8,25 +8,6 @@ import (
 
 func init() {
 	rootCmd.AddCommand(completionCmd)
-	rootCmd.BashCompletionFunction = `
-__munn_filename()
-{
-	if [[ ${#nouns[@]} -ge 1 ]]; then
-		return 1
-	fi
-	COMPREPLY=( $(compgen -W "$(find . -maxdepth 3 -name '*.munn')" -- $cur) )
-}
-
-__munn_custom_func() {
-	case ${last_command} in
-		munn)
-			__munn_filename
-			return
-			;;
-		*)
-			;;
-	esac
-}`
 }
 
 var completionCmd = &cobra.Command{
